2f7b076ab2795668a329fef9b32a72a40dcb190d
SEOKKAMONI <sj01048260279@gmail.com>
2022-12-05 12:20:22 +0900

    ADD :: chain

--- TYPECHAIN/src/index.ts
+++ TYPECHAIN/src/index.ts
@@ -1,6 +1,6 @@
 // 블록체인은 말 그대로 여러개의 블록이 사슬처럼 묶인것
 
-import crypto from "crypto";
+import crypto, { Hash } from "crypto";
 
 interface BlockShape {
     hash: string;
@@ -10,17 +10,51 @@ interface BlockShape {
 }
 
 class Block implements BlockShape {
-    public hash:string;
-    constructor (
+    public hash: string;
+    constructor(
         public prevHash: string,
         public height: number,
         public data: string
     ) {
         this.hash = Block.calculateHash(prevHash, height, data);
     }
-    static calculateHash(prevHash:string, height:number, data:string)
-    {
-        const toHash= `${prevHash}${height}${data}`;
+    static calculateHash(prevHash: string, height: number, data: string) {
+        const toHash = `${prevHash}${height}${data}`;
         return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
+
+class Blockchain {
+    private blocks: Block[]
+    constructor(
+
+    ) {
+        this.blocks = [];
+    }
+    private getPrevHash() {
+        if (this.blocks.length === 0) return "";
+        return this.blocks[this.blocks.length-1].hash;
+    }
+    public addBlock(data: string) {
+        const newBlock = new Block(
+            this.getPrevHash(), 
+            this.blocks.length + 1, 
+            data
+        );
+        this.blocks.push(newBlock);
+    }
+    public getBlocks() {
+        return [...this.blocks];
+    }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+
+blockchain.getBlocks().push(new Block("xxxxxx", 11111, "HACKEDDDDD"))
+
+console.log(blockchain.getBlocks())
